9f75b1dfd26c5a086b635615bb9af52fe3c6dcfc
jin <puny4me@citizen.seoul.kr>
2025-05-27 00:01:43 +0900

    first commit

---
+++ src/main/java/com/java/ilmondo/ilmondo_art/IlmondoArtApplication.java
@@ -0,0 +1,13 @@
+package com.java.ilmondo.ilmondo_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlmondoArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlmondoArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/ilmondo/ilmondo_art/IlmondoArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ilmondo.ilmondo_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlmondoArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
