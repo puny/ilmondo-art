0f54a24e1fa4f6845f75b4e2134cdaa4429ba5d7
superman <puny4me@hotmail.com>
2025-05-28 15:22:25 +0900

    하트 두개

--- src/main/java/com/java/ilmondo/ilmondo_art/Controller/HelloController.java
+++ src/main/java/com/java/ilmondo/ilmondo_art/Controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        String message = "Hello ilmondo ❤";
+        String message = "Hello ilmondo ❤❤";
         return message;
     }
 }
